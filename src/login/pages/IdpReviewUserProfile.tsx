bb6e70a5a2bf5b1c0d6ac2f70b7e2b5c7bae1842
Xavi <aguas.xavier2013a@gmail.com>
2025-03-21 12:01:06 -0500

    add feedback button

--- src/login/pages/IdpReviewUserProfile.tsx
+++ src/login/pages/IdpReviewUserProfile.tsx
@@ -7,7 +7,8 @@ import type { UserProfileFormFieldsProps } from "keycloakify/login/UserProfileFo
 import type { KcContext } from "../KcContext";
 import { checkboxVariants } from "../../components/ui/checkbox";
 import type { I18n } from "../i18n";
-import { buttonVariants } from "../../components/ui/button";
+import { Button, buttonVariants } from "../../components/ui/button";
+
 type IdpReviewUserProfileProps = PageProps<Extract<KcContext, { pageId: "idp-review-user-profile.ftl" }>, I18n> & {
     UserProfileFormFields: LazyOrNot<(props: UserProfileFormFieldsProps) => JSX.Element>;
     doMakeUserConfirmPassword: boolean;
@@ -27,8 +28,9 @@ export default function IdpReviewUserProfile(props: IdpReviewUserProfileProps) {
 
     const [isFomSubmittable, setIsFomSubmittable] = useState(false);
     const [areTermsAccepted, setAreTermsAccepted] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    
+
     return (
         <Template
             kcContext={kcContext}
@@ -39,7 +41,12 @@ export default function IdpReviewUserProfile(props: IdpReviewUserProfileProps) {
             displayRequiredFields
             headerNode={msg("loginIdpReviewProfileTitle")}
         >
-            <form id="kc-idp-review-profile-form" action={url.loginAction} method="post">
+            <form id="kc-idp-review-profile-form" action={url.loginAction} method="post" onSubmit={(e) => {
+                e.preventDefault();
+                setIsSubmitting(true);
+
+                (e.target as HTMLFormElement).submit();
+            }}>
                 <UserProfileFormFields
                     kcContext={kcContext}
                     i18n={i18n}
@@ -47,31 +54,68 @@ export default function IdpReviewUserProfile(props: IdpReviewUserProfileProps) {
                     kcClsx={kcClsx}
                     doMakeUserConfirmPassword={doMakeUserConfirmPassword}
                 />
-                {true && (
-                    <TermsAcceptance
-                        i18n={i18n}
-                        kcClsx={kcClsx}
-                        messagesPerField={messagesPerField}
-                        areTermsAccepted={areTermsAccepted}
-                        onAreTermsAcceptedValueChange={setAreTermsAccepted}
-                    />
-                )}
+                {
+                    true && (
+                        <TermsAcceptance
+                            i18n={i18n}
+                            kcClsx={kcClsx}
+                            messagesPerField={messagesPerField}
+                            areTermsAccepted={areTermsAccepted}
+                            onAreTermsAcceptedValueChange={setAreTermsAccepted}
+                        />
+                    )
+                }
                 <div className="p-5">
                     <div id="kc-form-options" className={kcClsx("kcFormOptionsClass")}>
                         <div className={kcClsx("kcFormOptionsWrapperClass")} />
                     </div>
                     <div id="kc-form-buttons" className="bg-blue">
-                        <input
+                        {/* <input
                             className={`${buttonVariants({ variant: "default" })} px-2 w-full`}
                             // className={kcClsx("kcButtonClass", "kcButtonPrimaryClass", "kcButtonBlockClass", "kcButtonLargeClass")}
                             type="submit"
                             value={msgStr("doSubmit")}
                             disabled={!isFomSubmittable || (!areTermsAccepted)}
-                        />
+                        /> */}
+                        <Button
+                            disabled={!isFomSubmittable || !areTermsAccepted || isSubmitting}
+                            type="submit"
+                            className="w-full"
+                            variant="default"
+                        >
+                            {isSubmitting ? (
+                                <>
+                                    <svg
+                                        className="animate-spin h-4 w-4 mr-2 text-white inline"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                                        ></path>
+                                    </svg>
+                                    Updating...
+                                </>
+                            ) : (
+                                "Submit"
+                            )}
+                        </Button>
+
                     </div>
                 </div>
             </form>
-        </Template>
+        </Template >
     );
 }
 
